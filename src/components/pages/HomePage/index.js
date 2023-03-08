354e14ee43a5e9ff3f8ef503a6f75e3e00906802
devgmz <devgmzz@gmail.com>
2023-03-09 00:41:55 +0900

    feat: Implement Homepage

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 
-import Header from './../../Organisms/Header/index';
-import MainSection from './../../Organisms/MainSection/index';
-import MoreButton from './../../Molecules/MoreButton/index';
-import Card from './../../Organisms/Card/index';
+import MoreButton from './../../Molecules/MoreButton';
+import Header from './../../Organisms/Header';
+import MainSection from './../../Organisms/MainSection';
+import CardGrid from './../../Organisms/CardGrid';
+
+import { moviesAPI, IMAGE_BASE_URL } from '../../../api/commonAxios';
 
 const HomePage = () => {
+  const [movieData, setMovieData] = useState( [] );
+  const [mainImagePath, setMainImagePath] = useState( null );
+  const [paramsPage, setParamsPage] = useState( 1 );
+
+  useEffect( () => {
+    getMovieData( paramsPage );
+  }, [] )
+
+  const getMovieData = useCallback( async (paramsPage) => {
+    const response = await moviesAPI.popular( paramsPage );
+    setMovieData( response.data.results);
+    setMainImagePath(`${IMAGE_BASE_URL}w1280${response.data.results[0].backdrop_path}`)
+    setParamsPage( response.data.page );
+  }, [] );
+
+  const loadMoreClick = async () => {
+    const response = await moviesAPI.popular( paramsPage + 1 );
+    setMovieData([...movieData, ...response.data.results])
+  }
   return (
     <>
       <Header />
-      <MainSection />
-      <Card />
-      <MoreButton palette='white' color='black' border='lightgray' />
+      <MainSection image={mainImagePath} movie={movieData} />
+      <CardGrid movie={movieData} />
+      <MoreButton palette="white" color="black" border="lightgray" event={loadMoreClick} />
     </>
-  )
+  );
 }
 
 export default HomePage
